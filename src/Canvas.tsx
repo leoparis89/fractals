7f54fce7c4bdbcb2a7d234942ba1896e5f2c1d5b
Lev Kowalski <levkowalski@gmail.com>
2020-11-11 23:49:48 +0100

    wip

--- src/Canvas.tsx
+++ src/Canvas.tsx
@@ -1,4 +1,5 @@
-import React, { useRef, useEffect } from "react";
+import React, { useRef, useState } from "react";
+import { pixelToPoint } from "./utils";
 
 const canvasStyle = {
   border: "1px solid blue",
@@ -10,13 +11,17 @@ type T = (event: React.PointerEvent<HTMLCanvasElement>) => void;
 
 export default function Canvas() {
   const canvasEl = useRef<HTMLCanvasElement>(null);
+  const [constant, setConstant] = useState<any>();
   const handleMove: T = event => {
     const rect = (event.target as any).getBoundingClientRect();
     const position = {
       x: event.clientX - rect.left,
       y: event.clientY - rect.top
     };
-    console.log(position);
+    const { im, re } = pixelToPoint(position.x, position.y);
+
+    setConstant([Math.round(re * 100) / 100, Math.round(im * 100) / 100]);
+
     // Get the mouse's XY coordinates on canvas
     //   const mouseX = event.clientX-canvas.offsetLeft
     //   mouseY = event.clientY-canvas.offsetTop
@@ -35,6 +40,7 @@ export default function Canvas() {
         onPointerMove={handleMove}
         style={canvasStyle}
       ></canvas>
+      {constant && <h1>{`${constant[0]} + ${constant[1]}i`}</h1>}
     </div>
   );
 }
