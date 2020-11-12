eb4f16c0f7d318f15f0a0a843ba2197d5407a2df
Lev Kowalski <levkowalski@gmail.com>
2020-11-12 22:29:50 +0100

    display complex number

--- src/utils.ts
+++ src/utils.ts
@@ -1,15 +1,15 @@
-import math from "mathjs";
+import { complex } from "mathjs";
 
 import config from "./config";
 const { width, height } = config.canvas;
 // the rest of your script...
 
 // Turn XY pixel coordinates into a point on the complex plane
-function pixelToPoint(x: number, y: number) {
+export function pixelToPoint(x: number, y: number) {
   // Map percentage of total width/height to a value from -1 to +1
   var zx = (x / width) * 2 - 1;
-  var zy = 1 - height * 2;
+  var zy = 1 - (y / height) * 2;
 
   // Create a complex number based on our new XY values
-  return math.complex(zx, zy);
+  return complex(zx, zy);
 }
