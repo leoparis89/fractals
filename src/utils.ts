bb00629d5ca2522100a5306c208a4a3847cb848e
Lev Kowalski <levkowalski@gmail.com>
2020-11-13 22:23:23 +0100

    wip

--- src/utils.ts
+++ src/utils.ts
@@ -18,11 +18,20 @@ export function pointToColor(point: math.Complex, constant?: math.Complex) {
   let newpoint = point;
   if (constant) {
     newpoint = math.divide(point, constant) as math.Complex;
+    var iterations = julia(point, constant);
+    const percentage = iterations / maxIterations;
+
+    var red = percentage * 255;
+    var green = percentage * 255;
+    var blue = percentage * 255;
+
+    return `rgb(${red}, ${green}, ${blue})`;
   }
-  var red = newpoint.re * 255;
-  var green = newpoint.im * 255;
-  var blue = (math.abs(point) as any) * 255;
-  return `rgb(${red}, ${green}, ${blue})`;
+  return "";
+  // How many iterations on this point before it escapes?
+  // var red = newpoint.re * 255;
+  // var green = newpoint.im * 255;
+  // var blue = (math.abs(point) as any) * 255;
 }
 
 export function drawPixel(
@@ -33,4 +42,22 @@ export function drawPixel(
 ) {
   ctx.fillStyle = color;
   ctx.fillRect(x, y, 1, 1);
+}
+
+export const maxIterations = 64;
+
+// Apply the Julia Set formula to see if point z "escapes"
+export function julia(z: math.Complex, c: math.Complex, i = 0): number {
+  // Apply the Julia Set formula: z*z+constant
+  const result = math.add(math.multiply(z, z), c) as math.Complex;
+
+  // Has our point escaped, or hit the iteration limit?
+  if (((math.abs(result) as any) as number) > 2 || i === maxIterations) {
+    return i;
+  }
+  // If so, return number of iterations
+  // If not, iterate again!
+  else {
+    return julia(result, c, i + 1);
+  }
 }
