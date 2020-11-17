75cd69ada5fac042d4270b2b7d12fa356e963f90
Lev Kowalski <levkowalski@gmail.com>
2020-11-17 17:30:33 +0100

    display fractals

--- src/Canvas.tsx
+++ src/Canvas.tsx
@@ -1,10 +1,11 @@
-import React, { useRef, useState } from "react";
-import { pixelToPoint } from "./utils";
+import React, { useRef, useState, useEffect } from "react";
+import { pixelToPoint, pointToColor, drawPixel } from "./utils";
+import config from "./config";
+
+const { height, width } = config.canvas;
 
 const canvasStyle = {
-  border: "1px solid blue",
-  width: 200,
-  height: 200
+  border: "1px solid blue"
 };
 
 type T = (event: React.PointerEvent<HTMLCanvasElement>) => void;
@@ -12,35 +13,56 @@ type T = (event: React.PointerEvent<HTMLCanvasElement>) => void;
 export default function Canvas() {
   const canvasEl = useRef<HTMLCanvasElement>(null);
   const [constant, setConstant] = useState<any>();
+
+  useEffect(() => {}, []);
+
   const handleMove: T = event => {
     const rect = (event.target as any).getBoundingClientRect();
     const position = {
       x: event.clientX - rect.left,
       y: event.clientY - rect.top
     };
-    const { im, re } = pixelToPoint(position.x, position.y);
+    const point = pixelToPoint(position.x, position.y);
 
-    setConstant([Math.round(re * 100) / 100, Math.round(im * 100) / 100]);
+    // setConstant([Math.round(re * 100) / 100, Math.round(im * 100) / 100]);
+    setConstant(point);
 
-    // Get the mouse's XY coordinates on canvas
-    //   const mouseX = event.clientX-canvas.offsetLeft
-    //   mouseY = event.clientY-canvas.offsetTop
-
-    //   // Turn mouse coordinates into a point on the complex plane
-    //   constant = pixelToPoint(mouseX, mouseY)
-
-    //   // Round that point off to the nearest 0.01
-    //   constant.re = math.round(constant.re*100)/100
-    //   constant.im = math.round(constant.im*100)/100
+    if (constant) {
+      var ctx = canvasEl.current!.getContext("2d")!;
+      draw(ctx, constant);
+    }
   };
+  const rounded = constant && [
+    Math.round(constant.re * 100) / 100,
+    Math.round(constant.im * 100) / 100
+  ];
   return (
     <div>
       <canvas
         ref={canvasEl}
         onPointerMove={handleMove}
         style={canvasStyle}
-      ></canvas>
-      {constant && <h1>{`${constant[0]} + ${constant[1]}i`}</h1>}
+        width={width}
+        height={height}
+      />
+      {constant && <h1>{`${rounded[0]} + ${rounded[1]}i`}</h1>}
     </div>
   );
+}
+
+function draw(ctx: CanvasRenderingContext2D, constant?: any) {
+  const { clientWidth, clientHeight } = ctx.canvas;
+  // Loop over every column of pixels
+  for (var y = 0; y < clientHeight; y++) {
+    // Loop over every row of pixels
+    for (var x = 0; x < clientWidth; x++) {
+      // Turn this pixel into a point in the complex plane
+      var point = pixelToPoint(x, y);
+
+      // Turn that point into a color
+      var color = pointToColor(point, constant);
+
+      drawPixel(ctx, x, y, color);
+    }
+  }
 }
--- src/utils.ts
+++ src/utils.ts
@@ -1,4 +1,4 @@
-import { complex } from "mathjs";
+import * as math from "mathjs";
 
 import config from "./config";
 const { width, height } = config.canvas;
@@ -11,5 +11,44 @@ export function pixelToPoint(x: number, y: number) {
   var zy = 1 - (y / height) * 2;
 
   // Create a complex number based on our new XY values
-  return complex(zx, zy);
+  return math.complex(zx, zy);
+}
+
+export function pointToColor(point: math.Complex, constant: math.Complex) {
+  let newpoint = point;
+  newpoint = math.divide(point, constant) as math.Complex;
+  var iterations = julia(point, constant);
+  const percentage = iterations / maxIterations;
+
+  var red = percentage * 255;
+  var green = percentage * 255;
+  var blue = percentage * 255;
+
+  return `rgb(${red}, ${green}, ${blue})`;
+}
+
+export function drawPixel(
+  ctx: CanvasRenderingContext2D,
+  x: number,
+  y: number,
+  color: string
+) {
+  ctx.fillStyle = color;
+  ctx.fillRect(x, y, 1, 1);
+}
+
+export const maxIterations = 64;
+
+// Apply the Julia Set formula to see if point z "escapes"
+export function julia(z: math.Complex, c: math.Complex, i = 0): number {
+  // Apply the Julia Set formula: z*z+constant
+  const result = math.add(math.multiply(z, z), c) as math.Complex;
+
+  // Has our point escaped, or hit the iteration limit?
+  if (((math.abs(result) as any) as number) > 2 || i === maxIterations) {
+    return i;
+  }
+  // If so, return number of iterations
+  // If not, iterate again!
+  return julia(result, c, i + 1);
 }
